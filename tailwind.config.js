2d7f536a6649afced92c9ce3f70b27ac22ff0c88
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-03-29 19:47:45 +0100

    push

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'winter-hazel': {
+          '50': '#faf9f0',
+          '100': '#f1efd4',
+          '200': '#e1dda6',
+          '300': '#d9d18e',
+          '400': '#c7b658',
+          '500': '#bc9e44',
+          '600': '#a68139',
+          '700': '#8a6433',
+          '800': '#72512e',
+          '900': '#5e4329',
+          '950': '#352313',
+      },
+      }
+    },
+  },
+  plugins: [],
+}
