a0c5d5424fc104b3e0cae94f370c3cb40e3bd83a
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-04-07 11:35:12 +0200

    push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,6 +16,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
     {
+        //$employe = new User();
+        //$employe -> setUsername('michel')->setRoles(['ROLE_SERVEUR'])->setPassword($hasher->hashPassword($employe,'0000'));
+        //$em->persist($employe);
+        //$em->flush();
+        
         return $this->render('home/index.html.twig', [
         ]);
     }
--- src/Controller/PanelGestionController.php
+++ src/Controller/PanelGestionController.php
@@ -3,18 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted(new Expression('is_granted("ROLE_ADMIN") or is_granted("ROLE_SERVEUR")'))]
 class PanelGestionController extends AbstractController
 {
     #[Route('/panel-gestion', name: 'app_panel_gestion')]
     public function index(Request $request): Response
     {
-
-        $this->denyAccessUnlessGranted('ROLE_USER');
-
         return $this->render('panel_gestion/index.html.twig', [
         ]);
     }
