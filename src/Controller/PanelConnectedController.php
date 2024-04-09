cad71d277e7b8cfed01ed432427fa5a749ed294b
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-04-09 16:15:38 +0200

    Update PanelConnectedController.php

--- src/Controller/PanelConnectedController.php
+++ src/Controller/PanelConnectedController.php
@@ -7,15 +7,22 @@ use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[IsGranted(new Expression('is_granted("ROLE_ADMIN") or is_granted("ROLE_SERVEUR")'))]
 class PanelConnectedController extends AbstractController
 {
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    
     #[Route('/panel-connected', name: 'app_panel_connected')]
     public function index(): Response
     {
         if ($this->isGranted("ROLE_ADMIN")) {
-            return $this->render('panel_gestion/index.html.twig');
+            return new RedirectResponse($this->urlGenerator->generate('app_panel_gestion'));
         }
         return $this->render('panel_gestion/index.html.twig');
     }
