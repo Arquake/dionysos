dfcb5ef9a7489599cc6a3a06dfa9697be3ba2b2b
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-04-18 15:00:28 +0200

    push du suicide
    je vais me harakiri

--- src/Controller/PanelGestionController.php
+++ src/Controller/PanelGestionController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Calendar;
 use App\Repository\CalendarRepository;
 use DateTime;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,21 +16,27 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class PanelGestionController extends AbstractController
 {
     #[Route('/panel-gestion', name: 'app_panel_gestion', methods: ['GET'])]
-    public function index(Request $request): Response
+    public function index(Request $request, CalendarRepository $calendar): Response
     {
         
         return $this->render('panel_gestion/index.html.twig', [
             'categories' => ['boissons','vins rouges', "vins rosés"],
-            'ouvert' => ['2023-01-12','2023-04-14','2023-04-14','2023-04-14','2023-04-14','2023-04-14'],
-            'fermer' => ['2023-01-06','2023-04-04']
+            'ouvert' => $calendar->findBy(['type' => 'ouvert']),
+            'fermer' => $calendar->findBy(['type' => 'fermer'])
         ]);
     }
 
     #[Route('/panel-gestion', name: 'app_panel_gestion.post', methods:['POST'])]
-    public function indexPost(Request $request, EntityManager $em, CalendarRepository $calendar): Response
+    public function indexPost(Request $request, EntityManagerInterface $em, CalendarRepository $calendar): Response
     {
         $payload = $request->getPayload();
         if($payload->get('gestion-submit-suppression') != null){
+            if ($this->isCsrfTokenValid('supprimer-calendrier', $payload->get('token'))){
+                if ($calendar->findOneBy(['date' => new \DateTime($payload->get('gestion-submit-suppression'))]) != null){
+                    $em->remove($calendar->findOneBy(['date' => new \DateTime($payload->get('gestion-submit-suppression'))]));
+                    $em->flush();
+                }
+            }
 
         } else if ($payload->get('gestion-submit') != null){
             switch($payload->get('gestion-submit')){
@@ -39,24 +45,44 @@ class PanelGestionController extends AbstractController
                     
                     }
                     break;
+
                 case 'ajoutCategorie':
                     if ($this->isCsrfTokenValid('ajouter-categorie', $payload->get('token'))){
-
-                    }
-                    break;
-                case 'ouvert':
-                    if ($this->isCsrfTokenValid('modifier-calendrier', $payload->get('token'))){
-                        if(new DateTime($payload->get('date')) >= (new DateTime('now')) && $calendar->findOneBy($payload->get('date')) == null){
+                        if($calendar->findBy(['nom' => $payload->get('nom')]) == []){
                             $dateOuverture = (new Calendar())
-                                ->setDate($payload->get('date'))
+                                ->setDate(new \DateTime($payload->get('date')))
                                 ->setType('ouvert');
-                            $em->persist();
+                            $em->persist($dateOuverture);
+                            $em->flush();
                         }
                     }
                     break;
+
+                case 'ouvert':
+                    if ($this->isCsrfTokenValid('modifier-calendrier', $payload->get('token'))){
+                        if(new DateTime($payload->get('date')) >= (new DateTime('now')) && 
+                        $calendar->findBy(['date' => new \DateTime($payload->get('date'))]) 
+                        == []){
+                            $dateOuverture = (new Calendar())
+                                ->setDate(new \DateTime($payload->get('date')))
+                                ->setType('ouvert');
+                            $em->persist($dateOuverture);
+                            $em->flush();
+                        }
+                    }
+                    break;
+
                 case 'fermer':
                     if ($this->isCsrfTokenValid('modifier-calendrier', $payload->get('token'))){
-
+                        if(new DateTime($payload->get('date')) >= (new DateTime('now')) && 
+                        $calendar->findBy(['date' => new \DateTime($payload->get('date'))]) 
+                        == []){
+                            $dateOuverture = (new Calendar())
+                                ->setDate(new \DateTime($payload->get('date')))
+                                ->setType('fermer');
+                            $em->persist($dateOuverture);
+                            $em->flush();
+                        }
                     }
                     break;
             }
@@ -64,12 +90,8 @@ class PanelGestionController extends AbstractController
         
         return $this->render('panel_gestion/index.html.twig', [
             'categories' => ['boissons','vins rouges', "vins rosés"],
-            'ouvert' => $calendar->findAll(['type' => 'ouvert']),
-            'fermer' => $calendar->findAll(['type' => 'fermer'])
+            'ouvert' => $calendar->findBy(['type' => 'ouvert']),
+            'fermer' => $calendar->findBy(['type' => 'fermer'])
         ]);
-    }
-
-    private function ajouterArticle(){
-
     }
 }
